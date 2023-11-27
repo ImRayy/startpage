f61d8de00885e669f0aae17b43f75a1578714796
RAY <souradeep20roy01@gmail.com>
2023-11-27 11:34:46 +0530

    Fix few texts are black in light mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="select-none">
+      <body className="select-none text-white">
         <Main />
         <NextScript />
       </body>
