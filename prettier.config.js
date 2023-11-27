7d27d16f7f3c8918631b16c7f02c4a5cb8ed3671
RAY <souradeep20roy01@gmail.com>
2023-11-27 11:37:41 +0530

    Refactor: Integrated Prettier plugins, ESLint, and Prettier configuration for code consistency

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  // REST OF YOUR CUSTOM PRETTIER CONFIG GOES BELOW
+  pluginSearchDirs: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
